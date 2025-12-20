77239e7f5283122f5362a4baddd803edbd89afc4
ishika-iisc <ishika.innovative@gmail.com>
2025-12-20 20:45:58 +0530

    Ansible controller and Jenkins server creation

--- DevOps-Project/infrastructure/main.tf
+++ DevOps-Project/infrastructure/main.tf
@@ -90,3 +90,174 @@ module "vpc" {
 # }
 
 
+
+# ---------------------------------------------------------------------------------------------------------------------
+# SECURITY GROUPS
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "ansible_controller_sg" {
+  name        = "ansible-controller-sg"
+  description = "Security group for Ansible Controller"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.custom_tags, {
+    Name = "ansible-controller-sg"
+  })
+}
+
+resource "aws_security_group" "jenkins_master_sg" {
+  name        = "jenkins-master-sg"
+  description = "Security group for Jenkins Master"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins web UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins agent communication"
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.custom_tags, {
+    Name = "jenkins-master-sg"
+  })
+}
+
+resource "aws_security_group" "jenkins_agent_sg" {
+  name        = "jenkins-agent-sg"
+  description = "Security group for Jenkins Agent"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description     = "SSH access"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description     = "Communication from Jenkins Master"
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jenkins_master_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.custom_tags, {
+    Name = "jenkins-agent-sg"
+  })
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# EC2 INSTANCES
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_instance" "ansible_controller" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = module.vpc.public_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.ansible_controller_sg.id]
+  key_name               = var.key_name
+
+  tags = merge(var.custom_tags, {
+    Name = "ansible-controller"
+    Role = "automation"
+  })
+}
+
+resource "aws_instance" "jenkins_master" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = module.vpc.public_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.jenkins_master_sg.id]
+  key_name               = var.key_name
+
+  tags = merge(var.custom_tags, {
+    Name = "jenkins-master"
+    Role = "ci-cd"
+  })
+}
+
+resource "aws_instance" "jenkins_agent" {
+  count                  = var.jenkins_agent_count
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = module.vpc.public_subnet_ids[count.index % length(module.vpc.public_subnet_ids)]
+  vpc_security_group_ids = [aws_security_group.jenkins_agent_sg.id]
+  key_name               = var.key_name
+
+  tags = merge(var.custom_tags, {
+    Name = "jenkins-agent-${count.index + 1}"
+    Role = "ci-cd-worker"
+  })
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OUTPUTS
+# ---------------------------------------------------------------------------------------------------------------------
+
+output "ansible_controller_public_ip" {
+  description = "Public IP of Ansible Controller"
+  value       = aws_instance.ansible_controller.public_ip
+}
+
+output "jenkins_master_public_ip" {
+  description = "Public IP of Jenkins Master"
+  value       = aws_instance.jenkins_master.public_ip
+}
+
+output "jenkins_master_url" {
+  description = "Jenkins Master URL"
+  value       = "http://${aws_instance.jenkins_master.public_ip}:8080"
+}
+
+output "jenkins_agent_public_ips" {
+  description = "Public IPs of Jenkins Agents"
+  value       = aws_instance.jenkins_agent[*].public_ip
+}
